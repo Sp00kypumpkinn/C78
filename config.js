dca7016c95a1dc3e22756dd417f5985ebf893ebb
Sp00kypumpkinn <104387856+Sp00kypumpkinn@users.noreply.github.com>
2023-02-13 16:00:03 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+var firebaseConfig = {
+  const firebaseConfig = {
+  apiKey: "AIzaSyCfTCK6lHxepbImBW7_9qRhPjkc7N-ERI8",
+  authDomain: "bicicleta-54dd5.firebaseapp.com",
+  projectId: "bicicleta-54dd5",
+  storageBucket: "bicicleta-54dd5.appspot.com",
+  messagingSenderId: "194114397014",
+  appId: "1:194114397014:web:ad0302e29db8322353acf7"
+};
+
+  
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
